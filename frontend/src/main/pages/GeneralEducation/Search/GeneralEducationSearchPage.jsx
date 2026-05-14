28327ab5d6eb3ca29c303e5ed5e42a79d594e7a9
Erik Rodriguez <erik_rodriguez@ucsb.edu>
2026-05-13 23:01:41 -0700

    Implemented GE Search Page

--- frontend/src/main/pages/GeneralEducation/Search/GeneralEducationSearchPage.jsx
+++ frontend/src/main/pages/GeneralEducation/Search/GeneralEducationSearchPage.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import GEAreaSearchForm from "main/components/GEAreas/GEAreaSearchForm";
+import GEAreaTable from "main/components/GEAreas/GEAreaTable";
+import { useBackendMutation } from "main/utils/useBackend";
 
 export default function GeneralEducationSearchPage() {
+  const [generalEducationJSON, setGeneralEducationJSON] = useState([]);
+
+  const objectToAxiosParams = (query) => ({
+    url: "/api/public/primariesge",
+    params: {
+      qtr: query.quarter,
+      area: query.area,
+    },
+  });
+
+  const onSuccess = (generalEducation) => {
+    setGeneralEducationJSON(generalEducation);
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // TODO: Remove this line and implement a test for caching
+    // Stryker disable next-line all : hard to set up test for caching
+    [],
+  );
+
+  async function fetchGeneralEducationJSON(_event, query) {
+    mutation.mutate(query);
+  }
   return (
     <BasicLayout>
-      <div className="container mt-3">
-        <h1>GE Search coming soon!</h1>
+      <div className="pt-2">
+        <h5>UCSB General Education Search</h5>
+        <GEAreaSearchForm fetchJSON={fetchGeneralEducationJSON} />
+        <GEAreaTable generalEducation={generalEducationJSON} schedules={[]} />
       </div>
     </BasicLayout>
   );
