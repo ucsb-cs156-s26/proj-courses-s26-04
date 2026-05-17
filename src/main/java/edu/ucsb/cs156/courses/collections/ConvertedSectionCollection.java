ec787ef7d906751e5e1e3dec92282b4aa6597902
Erik Rodriguez <erik_rodriguez@ucsb.edu>
2026-05-16 22:22:12 -0700

    Restored files to state on main to remove irrelavant files from PR

--- src/main/java/edu/ucsb/cs156/courses/collections/ConvertedSectionCollection.java
+++ src/main/java/edu/ucsb/cs156/courses/collections/ConvertedSectionCollection.java
@@ -23,11 +23,6 @@ public interface ConvertedSectionCollection extends MongoRepository<ConvertedSec
       String startQuarter, String endQuarter, String instructor, String functionCode);
 
   @Query(
-      "{'$or': [{'courseInfo.description': { $regex: ?0, $options: 'i' } }, {'courseInfo.title': { $regex: ?0, $options: 'i' } }], 'courseInfo.quarter': { $gte: ?1, $lte: ?2 }, 'section.section': { $regex: ?3 } }")
-  List<ConvertedSection> findBySearchTermsAndQuarterRange(
-      String searchTerms, String startQuarter, String endQuarter, String sectionRegex);
-
-  @Query(
       "{'courseInfo.quarter': { $gte: ?0, $lte: ?1 }, 'section.timeLocations.building': { $regex: ?2, $options: 'i' } }")
   List<ConvertedSection> findByQuarterRangeAndBuildingCode(
       String startQuarter, String endQuarter, String buildingCode);
