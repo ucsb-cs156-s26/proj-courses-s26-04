bd08783c87726d06d4a4d894e21bad405c879ad0
Daniel Jensen <djensen2@outlook.com>
2026-05-18 20:49:53 -0700

    dj - reset SecurityConfig to previous (no changes), consolidated CSRF tests

--- src/test/java/edu/ucsb/cs156/courses/config/SecurityConfigCsrfTests.java
+++ src/test/java/edu/ucsb/cs156/courses/config/SecurityConfigCsrfTests.java
@@ -10,29 +10,29 @@ import edu.ucsb.cs156.courses.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles("test")
 @WebMvcTest(controllers = CourseOverTimeController.class)
 @Import({SecurityConfig.class, RateLimitConfig.class})
-public class SecurityConfigNonDevelopmentCsrfTests {
+public class SecurityConfigCsrfTests {
 
-  @MockBean UserRepository userRepository;
+  @MockitoBean UserRepository userRepository;
 
-  @MockBean ConvertedSectionCollection convertedSectionCollection;
+  @MockitoBean ConvertedSectionCollection convertedSectionCollection;
 
   @Autowired private MockMvc mockMvc;
 
   @Test
-  public void post_without_csrf_is_forbidden_outside_development() throws Exception {
+  public void post_without_csrf_is_forbidden() throws Exception {
     mockMvc.perform(post("/api/public/courseovertime/search")).andExpect(status().isForbidden());
   }
 
   @Test
-  public void post_with_csrf_is_not_forbidden_outside_development() throws Exception {
+  public void endpoint_with_csrf_enabled_passes_with_csrf() throws Exception {
     mockMvc
         .perform(post("/api/public/courseovertime/search").with(csrf()))
         .andExpect(status().isMethodNotAllowed());
